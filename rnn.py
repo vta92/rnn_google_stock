8189652f7a4d1b61a85eabee4cfac71076a87578
Vinh Ta <vinhta92@gmail.com>
2017-10-13 01:05:31 -0700

    added visualization

--- rnn.py
+++ rnn.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import preprocessing
 
@@ -40,4 +40,29 @@ model.fit(x_train,y_train, batch_size=20, epochs=200)
 
 test_data=pd.read_csv('Google_Stock_Price_Test.csv')
 test_open=test_data.iloc[:,1:2]
-sc = preprocessing.MinMaxScaler((0,1))
+test_open = sc.fit_transform(test_open)
+
+x_test=test_open[0:1257,]
+y_test=test_open[1:1258,]
+
+x_test=np.reshape(x_test,(20,1,1))
+y_pred = model.predict(x_test,batch_size=20)
+#remember, we can only predict from the previous days...therefore the first/second day doesn't have much data
+
+
+########################
+#to graph
+
+def graph(real_open, y_pred):
+    y_pred = sc.inverse_transform(y_pred) #important, getting back the stock price from the standardscale
+    real_open = sc.inverse_transform(real_open)
+    plt.plot(real_open, color="black", label="Real Google Price")
+    plt.plot(y_pred, color="blue", label="predicted")
+    plt.title("prediction by lstm")
+    plt.xlabel("dates")
+    plt.ylabel("price")
+    plt.legend(loc="upper left")
+    plt.savefig('benchmark')
+    plt.show()
+    
+graph(test_open,y_pred) #bad naming scheme for test open, since this is all data, not just a test set
